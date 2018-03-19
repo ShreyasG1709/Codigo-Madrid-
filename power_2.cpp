f10980d86b8db81f6335c8957e4eaa22c14c1065
ShreyasG1709 <36772206+ShreyasG1709@users.noreply.github.com>
2018-03-19 23:20:49 +0530

    check integer in power of 2

---
+++ power_2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int num,n;
+cout<<"Enter a no. to check whteher it is in power of 2 "<<endl;
+cin>>num;
+n=num-1;
+if((num&n)==0)
+{
+cout<<"The given integer is in power of 2 "<<endl;
+}
+else{
+cout<<"Not in power of 2"<<endl;
+}
+return 0;
+}
